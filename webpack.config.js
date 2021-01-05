ccf4e55ec94031a51daf86018de1cdc7afa8c4bb
Alina Aprel <alina.aprelh@mail.ru>
2021-01-05 13:27:18 +0300

    plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require('webpack');
+
+
 module.exports = {
   entry: { main: './src/index.js' },
   output: {
@@ -11,7 +14,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.js$|jsx/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader"
@@ -19,12 +22,33 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use:  [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
-      }
+        use:  [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|gif|ico|svg)$/,
+        use: [
+                'file-loader?name=../images/[name].[ext]',
+                {
+                        loader: 'image-webpack-loader',
+                        options: {}
+                },
+        ]
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: [
+                 {
+                     loader: 'file-loader?name=./assets/fonts/webfonts/[name].[ext]'
+                 },
+                 {
+                     loader: 'file-loader?name=./assets/fonts/Roboto/[name].[ext]'
+                 }
+             ]
+    },
     ]
   },
   plugins: [
-    new MiniCssExtractPlugin({ //
+    new MiniCssExtractPlugin({
       filename: 'style.[contenthash].css',
     }),
     new HtmlWebpackPlugin({
@@ -32,6 +56,9 @@ module.exports = {
       template: './src/index.html',
       filename: 'index.html'
     }),
-    new WebpackMd5Hash()
+    new WebpackMd5Hash(),
+    new webpack.DefinePlugin({
+      'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  })
   ]
 };
